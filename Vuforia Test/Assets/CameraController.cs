7a94349db07a0f6ca0a0fd570a7999d3334659f8
GAWSSODIA\LuisMiguel <luispalacio1997@hotmail.com>
2017-06-15 21:47:43 -0500

    División extraña por corregir

---
+++ Vuforia Test/Assets/CameraController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour
+{
+    public WebCamTexture mCamera = null;
+    public GameObject plane;
+    //public GameObject plane_2;
+
+    // Use this for initialization
+    void Start()
+    {
+        plane = GameObject.FindWithTag("Player");
+        //plane_2 = GameObject.FindWithTag("Player_2");
+
+        mCamera = new WebCamTexture();
+        plane.GetComponent<Renderer>().material.mainTexture = mCamera;
+        //plane_2.GetComponent<Renderer>().material.mainTexture = mCamera;
+        mCamera.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
